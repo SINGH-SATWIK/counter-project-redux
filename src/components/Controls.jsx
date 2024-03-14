2fe53e58e1ea28e68251becd0ea5ffdec89e2371
SINGH-SATWIK <119724007+SINGH-SATWIK@users.noreply.github.com>
2024-03-14 20:59:50 +0530

    Update Controls.jsx

--- src/components/Controls.jsx
+++ src/components/Controls.jsx
@@ -36,13 +36,13 @@ const Controls=()=>{
 
     return(
         <>
-            <div classNameName="d-grid gap-2 d-sm-flex justify-content-sm-center">
+            <div className="d-grid gap-2 d-sm-flex justify-content-sm-center">
                 <button type="button" className="btn btn-primary"  onClick={handleIncrement}>+1</button>
                 <button type="button" className="btn btn-success"  onClick={handleDercrement}>-1</button>
                 <button type="button" className="btn btn-warning"  onClick={handlePrivacyToggle}>Privacy Toggle</button>
             </div>
 
-            <div classNameName="d-grid gap-2 d-sm-flex justify-content-sm-center control-row">
+            <div className="d-grid gap-2 d-sm-flex justify-content-sm-center control-row">
                 <input type="text" placeholder="Enter a number" className="number-input"  ref={inputElement}/>
                 <button type="button" className="btn btn-info"  onClick={handleAdd}>Add</button>
                 <button type="button" className="btn btn-danger"  onClick={handleSubtsract}>Substract</button>
